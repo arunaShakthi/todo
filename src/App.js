852f2061906820ce66164f3f7351de4eb65d550c
arunaShakthi <arunashakthi002@gmail.com>
2023-03-06 20:29:21 +0530

    DB connect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-const Style ={
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[1CB5E0]`
+const style ={
+  bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[1CB5E0]`,
 }
 
 function App() {
   return (
-    <div className={Style.bg}>
-      
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>ToDo App</h3>
+        <form className={style.form}>
+          
+        </form>
+      </div>
     </div>
   );
 }
